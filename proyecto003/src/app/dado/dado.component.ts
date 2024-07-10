0ebe6e48d03975c9608967f24e00155e69d4d7d4
chris-jt <chtronconi@gmail.com>
2024-07-10 16:04:08 -0300

    segundo commit

--- proyecto003/src/app/dado/dado.component.ts
+++ proyecto003/src/app/dado/dado.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
   templateUrl: './dado.component.html',
   styleUrl: './dado.component.css'
 })
-export class DadoComponent {
-  valor: number=1;
+export class DadoComponent implements OnInit{
+  //interpolacion
+  @Input() valor: string="";
+//property binding
+  @Input() numero: number=1;
+
   constructor(){}
 
   ngOnInit(){
-    this.valor = Math.trunc((Math.random() * 6 )) + 1;
+   
   }
 }
